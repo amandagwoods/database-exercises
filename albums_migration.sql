b20b4a22d694418c0fa175be6d87360cd581911b
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-27 11:29:21 -0500

    feat: basic statements, building file

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(40) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    release_date DATE,
+    name VARCHAR(75) NOT NULL,
+    release_date INT,
+    genre VARCHAR(50),
     sales FLOAT,
-    genre VARCHAR(20),
     PRIMARY KEY(id)
 );
