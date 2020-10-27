26da86be07745b33852629e3c3e2708bae085415
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-27 12:28:20 -0500

    feat: basic statements, delete

---
+++ delete_exercises.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+# Albums released after 1991
+SELECT * FROM albums WHERE release_date < 1991;
+DELETE FROM albums WHERE release_date < 1991;
+# Albums with the genre 'disco'
+SELECT * FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
+# Albums by 'Whitney Houston'
+# (...or maybe an artist of your choice)
+SELECT * FROM albums WHERE artist = 'Fleetwood Mac';
+DELETE FROM albums WHERE artist = 'Fleetwood Mac';
