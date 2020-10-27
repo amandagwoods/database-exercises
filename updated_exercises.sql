4eaef79e8e1ea5576caa5fbece55ef42d2102e4a
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-27 12:15:50 -0500

    feat: basic statements, updates...updated

--- updated_exercises.sql
+++ updated_exercises.sql
@@ -12,7 +12,10 @@ SELECT * FROM albums where artist = 'Michael Jackson';
 #
 # Make all the albums 10 times more popular (sales * 10)
 UPDATE albums SET sales = (sales * 10);
+SELECT * from albums;
 # Move all the albums before 1980 back to the 1800s.
-UPDATE albums SET release_date = (release_date < 1980 = 1800);
+UPDATE albums SET release_date = (release_date - 100) WHERE release_date < 1980;
+SELECT * from albums;
 # Change 'Michael Jackson' to 'Peter Jackson'
-UPDATE albums SET artist = ('Michael Jackson' = 'Peter Jackson');
+UPDATE albums SET artist = ('Peter Jackson') WHERE artist = 'Michael Jackson';
+SELECT * FROM albums where artist = 'Peter Jackson';
